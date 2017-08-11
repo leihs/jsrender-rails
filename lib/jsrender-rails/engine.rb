922edbe0bb15ce5a181de0de839c069755ff985a
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-08-11 17:39:02 +0200

    remove initializer

--- lib/jsrender-rails/engine.rb
+++ lib/jsrender-rails/engine.rb
@@ -4,11 +4,5 @@ module JsrenderRails
   class Railtie < Rails::Engine
     config.jsrender = ActiveSupport::OrderedOptions.new
     config.jsrender.prefix = ""
-
-    config.after_initialize do |app|
-      if app.assets
-        app.assets.register_engine(".jsr", Jsrender)
-      end
-    end
   end
 end
